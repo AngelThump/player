157e1269ab50453658884982091d7e0cf9b40f11
TimIsOverpowered <admin@angelthump.com>
2020-10-13 16:51:46 +0000

    fix storage

--- src/storage.js
+++ src/storage.js
@@ -1,49 +1,55 @@
 let localStorage;
+let sessionStorage;
 
 try {
   localStorage = window.localStorage;
-} catch(e) {
-}
+  sessionStorage = window.sessionStorage;
+} catch(e) {}
 
 export function localStorageGetItem(key) {
   try {
     return localStorage.getItem(key);
-  } catch (e) {}
+  } catch (e) {
+    return null;
+  }
 }
 
 export function localStorageRemoveItem(key) {
   try {
     localStorage.removeItem(key);
-  } catch (e) {}
+  } catch (e) {
+    return null;
+  }
 }
 
 export function localStorageSetItem(key, value) {
   try {
     return localStorage.setItem(key, value);
-  } catch (e) {}
-}
-
-let sessionStorage;
-
-try {
-  sessionStorage = window.sessionStorage;
-} catch(e) {
+  } catch (e) {
+    return null;
+  }
 }
 
 export function sessionStorageGetItem(key) {
   try {
     return sessionStorage.getItem(key);
-  } catch (e) {}
+  } catch (e) {
+    return null;
+  }
 }
 
 export function sessionStorageRemoveItem(key) {
   try {
     sessionStorage.removeItem(key);
-  } catch (e) {}
+  } catch (e) {
+    return null;
+  }
 }
 
 export function sessionStorageSetItem(key, value) {
   try {
     return sessionStorage.setItem(key, value);
-  } catch (e) {}
+  } catch (e) {
+    return null;
+  }
 }
